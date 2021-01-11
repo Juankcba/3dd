44c96c5ed8387d6a79af9bde628f284934bd39bd
juankcba <juangblade@hotmail.com>
2021-01-10 23:22:56 -0300

    sin carrousel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React, { useMemo, useState, useEffect } from "react";
 import Router from "next/router";
-import "../styles/global/globals.css";
+
 import "semantic-ui-css/semantic.min.css";
 
 import "../styles/sass/main.scss";
